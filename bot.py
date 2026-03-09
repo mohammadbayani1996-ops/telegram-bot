ca1f510c6f47b900589d3b127f52b682000e3a09
mohammadbayani1996-ops <mohammadbayani1996@gmail.com>
2026-03-09 02:45:05 +0100

    Refactor bot.py by cleaning up unused code
    Removed unused channels and song data, and simplified the start command logic.

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = TOKEN = "8571831791:AAFTZFJaytKS8djih7ZwVadw2kykGnVyHug"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🎧 بەخێربێیت بۆ رباتی ساتــێـکے خــۆش")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot running...")
+app.run_polling()

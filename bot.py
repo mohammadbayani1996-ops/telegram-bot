f65cca0023da1313f20667a5b7f13715f879cfe6
mohammadbayani1996-ops <mohammadbayani1996@gmail.com>
2026-03-09 04:13:45 +0100

    Update bot.py with new token and start message

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = TOKEN = "8571831791:AAFTZFJaytKS8djih7ZwVadw2kykGnVyHug"
+TOKEN = " 8571831791:AAFTZFJaytKS8djih7ZwVadw2kykGnVyHug"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎧 بەخێربێیت بۆ رباتی ساتــێـکے خــۆش")
+    await update.message.reply_text("ربات فعال شد!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
